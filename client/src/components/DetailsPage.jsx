65431b6585f7f09fe95b4576c0df8b9de6c4b6d9
Girish keleng <girishkeleng30@gmail.com>
2024-08-18 16:12:36 +0530

    DetailspageEdit1

--- client/src/components/DetailsPage.jsx
+++ client/src/components/DetailsPage.jsx
@@ -20,51 +20,113 @@ export default function DetailsPage() {
 
             }
         }
+        if (details.length === 0) {
+            fetchDetails();
+        }
 
-        fetchDetails();
-    }, [])
+    }, [id])
+
 
     return (
         <div>
             <Header />
-            <h1 className="text-3xl"> {details.title} </h1>
+            <div className="w-9/12  mx-auto">
 
-           
+                <div className="grid gap-6 my-6">
+                    <div>  <h1 className="text-6xl"> {details.title} </h1> </div>
 
-            <div className="grid grid-cols-[2fr_1fr]">
+                    <div className="grid lg:grid-cols-[3fr_2fr] gap-12">
 
-           <div className="grid grid-cols-[3fr_2fr]">
 
-           
-            <div className="grid grid-cols-3 bg-orange-300 px-6">
-            {details.extendedIngredients && details.extendedIngredients.map((ingredient, inx) => (
-                <div key={inx} className="m-2  ">
-                    <p>Alise: {ingredient.aisle} </p>
-                  <p> Name: {ingredient.name} </p>  
-                  {/* <p>Amount : {ingredient.amount} </p> */}
-                  {/* <p>Consistency : {ingredient.consistency} </p> */}
-                  <p>original :{ingredient.original} </p>
-                    
-                  
-                </div>
-            ))}
-            </div>
-            <div className="bg-yellow-100">
-                <img src={details.image} alt={details.title} />
-            </div>
-            </div>
+                        <div className=" h-min grid">
 
-            <div className="w-full bg-green-200">
-              {details.nutrition && details.nutrition.nutrients.map((nutrient, inx) => (
-                    <div key={inx} className="m-2">
-                        <p> Name: {nutrient.name}  </p>
-                        <p>Amount : {nutrient.amount}</p>
-                        <p>PercentOfDailyNeeds: {nutrient.unit} </p>
+                            <img className="w-full object-cover" src={details.image} alt={details.title} />
+
+                            <div className="grid grid-cols-4  w-full">
+
+                                {details.nutrition &&
+                                    details.nutrition.nutrients
+                                        .filter(nutrient =>
+                                            ["Calories", "Protein", "Fat", "Carbohydrates"].includes(nutrient.name)
+                                        )
+                                        .map((nutrient, inx) => (
+                                            <div key={inx} className="m-2  ">
+                                                {nutrient.name === "Calories" && <p className="grid text-sm">Calories: <span className="text-2xl">{nutrient.amount}{nutrient.unit} </span></p>}
+                                                {nutrient.name === "Protein" && <p className="grid text-sm">Protein: <span className="text-2xl">{nutrient.amount}{nutrient.unit} </span></p>}
+                                                {nutrient.name === "Fat" && <p className="grid text-sm">Fat: <span className="text-2xl">{nutrient.amount}{nutrient.unit} </span></p>}
+                                                {nutrient.name === "Carbohydrates" && <p className="grid text-sm">Carbs: <span className="text-2xl">{nutrient.amount}{nutrient.unit} </span></p>}
+                                            </div>
+                                        ))
+                                }
+
+                            </div>
+                        </div>
+
+                        <div className="">
+                            <h1 className="text-3xl mb-6">Ingredients:</h1>
+                            {details.extendedIngredients && details.extendedIngredients.map((ingredient, inx) => (
+                                <div key={inx} className="m-2  ">
+                                    
+                                     
+                                    <p className="capitalize">{ingredient.original} </p>
+
+
+                                </div>
+                            ))}
+                        </div>
+
                     </div>
-              ))}
+                </div>
             </div>
-           
-            </div>
+
+
+
         </div>
     )
+    // return (
+    //     <div>
+    //         <Header />
+    //         <h1 className="text-3xl"> {details.title} </h1>
+
+
+
+    //         <div className="grid lg:grid-cols-[2fr_1fr] gap-4">
+
+    //        <div className="grid grid-cols-[3fr_2fr]">
+
+
+    //         <div className="grid grid-cols-3 px-6 h-min">
+    //         {details.extendedIngredients && details.extendedIngredients.map((ingredient, inx) => (
+    //             <div key={inx} className="m-2  ">
+    //                 <p>Alise: {ingredient.aisle} </p>
+    //               <p> Name: {ingredient.name} </p>  
+    //               {/* <p>Amount : {ingredient.amount} </p> */}
+    //               {/* <p>Consistency : {ingredient.consistency} </p> */}
+    //               <p>original :{ingredient.original} </p>
+
+
+    //             </div>
+    //         ))}
+    //         </div>
+    //         <div className="bg-yellow-100 h-min">
+    //             <img src={details.image} alt={details.title} />
+    //         </div>
+    //         </div>
+    //       <div>
+
+    //      <p className="text-3xl">Nutrition values:</p>
+    //         <div className="w-full  grid grid-cols-3">
+
+    //           {details.nutrition && details.nutrition.nutrients.map((nutrient, inx) => (
+    //                 <div key={inx} className="m-2">
+    //                     <p> Name: {nutrient.name}  </p>
+    //                     <p>Amount : {nutrient.amount}{nutrient.unit}</p>
+    //                     <p>DailyNeeds: {nutrient.percentOfDailyNeeds}% </p>
+    //                 </div>
+    //           ))}
+    //         </div>
+    //         </div>
+    //         </div>
+    //     </div>
+    // )
 }

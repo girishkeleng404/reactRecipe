a702587c9944030e1570c84a3678d7f5a4d7c357
Girish keleng <girishkeleng30@gmail.com>
2024-08-19 18:57:16 +0530

    phoneOptimize1

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,18 +1,46 @@
+import { useEffect, useState } from "react";
 import Header from "./Headers/Header";
 import SearchSm from "./SearchSm";
 
 
 export default function Search() {
-  
-    return(
+
+    const [listData, setListData] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:4000/dishs", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                })
+                const data = await response.json();
+                console.log(data);
+                setListData(data);
+                ;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+
+    }, [])
+
+
+    return (
         <div>
             <div>
-                <Header/>
+                <Header />
             </div>
             <div>
-                <SearchSm/>
+                <SearchSm />
             </div>
         </div>
     )
-   
+
 }
--- client/src/components/SearchSm.jsx
+++ client/src/components/SearchSm.jsx
@@ -73,39 +73,42 @@ export default function SearchSm() {
                             onKeyDown={(ev) => { if (ev.key === "Enter") fetchData() }}
                         />
                     </div>
-                    <div className="lg:hidden bg-blue-400 py-1 px-4 rounded-lg cursor-pointer" onClick={fetchData}>Search</div>
+                    <div className="lg:hidden text-lg py-1 px-4 rounded-lg cursor-pointer" onClick={fetchData}>Search</div>
 
                     <div>
                         <div className=" py-1 px-4 rounded-lg cursor-pointer text-lg  " onClick={filterToggle}>Filter</div>
                     </div>
                 </div>
                 {filter && (
-                    <div className="grid grid-cols-3 gap-6">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 mx-2">
 
 
-                        <label className="gap-4" htmlFor="">Cuisine :
+                        <label className="gap-4 flex justify-between items-baseline" htmlFor="">Cuisine :
                             <input onChange={(ev) => setCuisine(ev.target.value)} type="text" placeholder="italian" className="border border-gray-400 outline-none px-1 rounded-md h-8 ml-2 " />
                         </label>
 
 
-                        <label className="gap-4" htmlFor="">Diet :
+                        <label className="gap-4 flex justify-between items-baseline" htmlFor="">Diet :
                             <input onChange={(ev) => setDiet(ev.target.value)} type="text" placeholder="vegetarian" className="border border-gray-400 outline-none px-1 rounded-md h-8 ml-2 " />
                         </label>
-                        <label className="gap-4" htmlFor="">Type :
+                        <label className="gap-4 flex justify-between items-baseline" htmlFor="">Type :
                             <input onChange={(ev) => setType(ev.target.value)} type="text" placeholder="main course" className="border border-gray-400 outline-none px-1 rounded-md h-8 ml-2 " />
                         </label>
-                        <label onChange={(ev) => setIncludeIngredients(ev.target.value)} className="gap-4" htmlFor="">Include Ingredients  :
+                        <label onChange={(ev) => setIncludeIngredients(ev.target.value)} className="gap-4 flex justify-between items-baseline" htmlFor="">Include Ingredients  :
                             <input type="text" className="border border-gray-400 outline-none px-1 rounded-md h-8 ml-2 " />
                         </label>
-                        <label onChange={(ev) => setExcludeIngredients(ev.target.value)} className="gap-4" htmlFor="">Exclude Ingredients :
+                        <label onChange={(ev) => setExcludeIngredients(ev.target.value)} className="gap-4 flex justify-between items-baseline" htmlFor="">Exclude Ingredients :
                             <input type="text" className="border border-gray-400 outline-none px-1 rounded-md h-8 ml-2 " />
                         </label>
-                        <div
-                            className="hidden lg:block text-center text-lg  px-4 rounded-lg cursor-pointer border border-gray-500 max-w-48"
+                        <div className="flex justify-end items-center">
+                            <div
+                            className="hidden lg:block md:block text-center text-lg py-0.5 px-4 rounded-lg cursor-pointer border border-gray-500 w-48 bg-black/80 text-white "
                             onClick={fetchData}
                         >
                             Search
                         </div>
+                        </div>
+                        
 
                     </div>
                 )}

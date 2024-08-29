88baf63aa4b7f5d65bba85606b949ee447dc6690
Girish keleng <girishkeleng30@gmail.com>
2024-08-29 22:48:13 +0530

    chore: Update T&P page footer stylin
    - Remove dark background class from T&P page fgooter for better consistency with other pages

--- client/src/components/Footers/T&P.jsx
+++ client/src/components/Footers/T&P.jsx
@@ -10,7 +10,7 @@ export default function TnP() {
 
 
     return (
-        <div className="h-screen items-center justify-center dark:bg-neutral-900">
+        <div className="h-screen items-center justify-center">
 
 
          <ErrorFun/>

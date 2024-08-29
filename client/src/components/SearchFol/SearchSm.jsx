6fd0df8ace9c1ba15e765ba846a9afe31c6cc3cb
Girish keleng <girishkeleng30@gmail.com>
2024-08-29 23:06:47 +0530

    Update footer links to include News and GitHub pages

--- client/src/components/SearchFol/SearchSm.jsx
+++ client/src/components/SearchFol/SearchSm.jsx
@@ -64,7 +64,7 @@ export default function SearchSm() {
 
 
             <div className="flex justify-center flex-col items-center h-5/6 gap-8 z-10 mt-20">
-                <div className="flex  w-screen border border-gray-500 dark:border-none ">
+                <div className="flex w-screen border border-gray-500 dark:border-none ">
                     <div className="gap-8 w-full ml-8 py-1 lg:py-2">
 
                         <input

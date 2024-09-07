9aacd514fdc98045f36c61a1cea57dd4e4cbb0c3
Girish keleng <girishkeleng30@gmail.com>
2024-09-08 00:02:48 +0530

    Refactor Cards component to use consistent spacing and formatting

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -65,8 +65,8 @@ export default function Cards({ recipesPerPage, data, gridClassName, x, y }) {
 
 
 
-                                <div className="relative group duration-500 cursor-pointer group overflow-hidden  text-gray-50 h-72 w-56  rounded-2xl hover:duration-700 ">
-                                    <div className="w-56 h-72 bg-orange-400 text-gray-800">
+                                <div className="relative group duration-500 cursor-pointer group overflow-hidden  text-gray-50 h-72 w-60  rounded-2xl hover:duration-700 ">
+                                    <div className="w-60 h-80  text-gray-800">
                                         <div className="flex flex-row justify-between">
                                             <img
                                                 src={dish.image}
@@ -113,11 +113,11 @@ export default function Cards({ recipesPerPage, data, gridClassName, x, y }) {
                                             </svg>
                                         </div>
                                     </div>
-                                    <div className="absolute bg-gray-50 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
-                                        <span className="text-lime-400 font-bold text-xs">TAILWIND</span>
-                                        <span className="text-gray-800 font-bold text-3xl">Cheat Sheet</span>
+                                    <div className="absolute bg-gray-50 -bottom-24 w-60 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
+                                        {/* <span className="text-lime-400 font-bold text-xs">Pies.com</span> */}
+                                        <span className="text-gray-800 font-bold text-2xl"> {dish.title}</span>
                                         <p className="text-neutral-800">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        {/* {dish.title} */}
                                         </p>
                                     </div>
                                 </div>

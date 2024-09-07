98875cf6b4469072ef696aac0f235ef442876c3d
Girish keleng <girishkeleng30@gmail.com>
2024-09-07 23:48:38 +0530

    Add CardTailwind component to DetailsFolder and import it in News component

--- client/src/components/Footers/News.jsx
+++ client/src/components/Footers/News.jsx
@@ -1,9 +1,11 @@
+import CardTailwind from "../DetailsFolder/CardTailwind";
 import ErrorFun from "./error";
 
 export default function News(){
     return(
         <div>
         <ErrorFun/>
+        <CardTailwind/>
         </div>
     )
 }

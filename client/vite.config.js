3215772f9f732d1af92f8f3bd5f52d4c3883fdb8
Girish keleng <girishkeleng30@gmail.com>
2024-09-09 23:19:47 +0530

    Refactor package.json and yarn.lock files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
  
   optimizeDeps: {
     exclude: ['chunk-IU5JCZQL', 'chunk-HKJ2B2AA'], // Add the problematic chunks here
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })

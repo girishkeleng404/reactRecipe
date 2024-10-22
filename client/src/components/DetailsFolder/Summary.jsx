ad2f566149ae611bd7da457c23999816c6a5dffa
Girish keleng <girishkeleng30@gmail.com>
2024-10-22 20:53:31 +0530

    Refactor Summary component to remove unnecessary whitespace and add missing line breaks

--- client/src/components/DetailsFolder/Summary.jsx
+++ client/src/components/DetailsFolder/Summary.jsx
@@ -1,11 +1,19 @@
 export default function Summary({ details }) {
+
+    
     return (
+
+
         <div className="py-6 px-4">
+
+
 
             <div
                 className="summary dark:text-gray-300"
                 dangerouslySetInnerHTML={{ __html: details.summary }}
             />
+
+            
 
         </div>
 
